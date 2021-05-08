5732546ab4c38f7b113d1071f16afcee947d9ac8
Larry <larrylee3107@gmail.com>
2021-05-09 03:59:04 +0800

    tiktok download

--- src/helpers/CommonFunctions.jsx
+++ src/helpers/CommonFunctions.jsx
@@ -109,51 +109,66 @@ export const convertUsernameToSocialLink = (socialType, userIdentifier) => {
 };
 
 export const downloadAndSaveTikToks = async () => {
-  let res;
-  for (let i = 0; i < 6; i++) {
-    console.log("download try count " + i);
-    if (i == 6) {
-      axios.post("/v1/email/severeError", {
-        userId: localStorage.getItem("USER_ID"),
-        userName: localStorage.getItem("USER_NAME"),
-      });
-
-      alert(
-        "We are unable to download your content now. We are working on it and will contact you via email when solved."
-      );
-
-      break;
-    }
-
+  try {
+    let res;
     console.log("getting tiktoks info");
-    try {
-      res = await axios.get(
-        "v1/tiktok/getInfo/" + localStorage.getItem("USER_ID")
-      );
+    res = await retryPromiseWithDelay(
+      axios.get("v1/tiktok/getInfo/" + localStorage.getItem("USER_ID")),
+      5,
+      10000
+    );
 
-      try {
-        console.log("downloading tiktoks");
-        res = await axios.get(
-          "v1/tiktok/download/" + localStorage.getItem("USER_ID")
-        );
+    console.log("downloading tiktoks");
 
-        try {
-          console.log("saving tiktoks");
-          res = await axios.get(
-            "v1/tiktok/saveTikToks/" + localStorage.getItem("USER_ID")
-          );
+    res = await retryPromiseWithDelay(
+      axios.get("v1/tiktok/download/" + localStorage.getItem("USER_ID")),
+      5,
+      10000
+    );
 
-          if (res.status == 200) {
-            return "success";
-          }
-        } catch {
-          continue;
-        }
-      } catch {
-        continue;
-      }
-    } catch {
-      continue;
-    }
+    console.log("saving tiktoks");
+
+    res = await retryPromiseWithDelay(
+      axios.get("v1/tiktok/saveTikToks/" + localStorage.getItem("USER_ID")),
+      5,
+      10000
+    );
+
+    console.log(res);
+
+    return "success";
+  } catch {
+    axios.post("/v1/email/severeError", {
+      userId: localStorage.getItem("USER_ID"),
+      userName: localStorage.getItem("USER_NAME"),
+    });
+
+    alert(
+      "We are unable to download your content now. We are working on it and will contact you via email when solved."
+    );
+
+    return "failure";
   }
 };
+
+function waitFor(millSeconds) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, millSeconds);
+  });
+}
+async function retryPromiseWithDelay(promise, nthTry, delayTime) {
+  try {
+    const res = await promise;
+    return res;
+  } catch (e) {
+    if (nthTry === 1) {
+      return Promise.reject(e);
+    }
+    console.log("retrying", nthTry, "time");
+    // wait for delayTime amount of time before calling this method again
+    await waitFor(delayTime);
+    return retryPromiseWithDelay(promise, nthTry - 1, delayTime);
+  }
+}
