c3a5e53178ae2b0579a5ed1b6f2cc0c3fcc2391b
Larry <larrylee3107@gmail.com>
2021-05-09 00:41:29 +0800

    try z index

--- src/login/StayLoginForm.jsx
+++ src/login/StayLoginForm.jsx
@@ -135,22 +135,38 @@ const StayLoginForm = (props) => {
   // MANUAL SIGN UP
   if (manualForm) {
     return (
-      <div className="formMainLocal">
-        {!focused && (
-          <div className="Form_ManualWelcomeMessageWrapper">
-            <p className="Form_WelcomeWord">Vosh</p>
-            <img
-              src="https://media2locoloco-us.s3.amazonaws.com/ShopLocoLoco+Small+Symbol+Orange.png"
-              alt="loco logo"
-              style={{ height: 20 }}
-            />
-            {signUp ? (
-              <p className="Form_WelcomeText">Sign Up</p>
-            ) : (
-              <p className="Form_WelcomeText">Sign In</p>
-            )}
-          </div>
-        )}
+      <div className="formMainLocal" style={focused ? { zIndex: -1 } : null}>
+        <div
+          className="Form_ManualWelcomeMessageWrapper"
+          style={focused ? { zIndex: -1 } : null}
+        >
+          <p
+            className="Form_WelcomeWord"
+            style={focused ? { zIndex: -1 } : null}
+          >
+            Vosh
+          </p>
+          <img
+            src="https://media2locoloco-us.s3.amazonaws.com/ShopLocoLoco+Small+Symbol+Orange.png"
+            alt="loco logo"
+            style={focused ? { zIndex: -1 } : { height: 20 }}
+          />
+          {signUp ? (
+            <p
+              className="Form_WelcomeText"
+              style={focused ? { zIndex: -1 } : null}
+            >
+              Sign Up
+            </p>
+          ) : (
+            <p
+              className="Form_WelcomeText"
+              style={focused ? { zIndex: -1 } : null}
+            >
+              Sign In
+            </p>
+          )}
+        </div>
 
         {signUp ? (
           <div className="loginButtonWrapperLocal">
